4e3cc7a8349a59e2208d7485f4d1dd86a2beeced
elumori <elumori8257@gmail.com>
2022-09-22 16:15:28 +0200

    7-edges commit

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = "Hol"
-word_last_2 = "on"
-word_middle = "bert"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+word_middle = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {word_middle}")
