939e66af24b761d308272b9763588b84a63b6c9b
elumori <elumori8257@gmail.com>
2022-10-25 16:25:41 +0200

    add save commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-# 9-add_item.py
-# Carlos Barros <1543@holbertonschool.com>
-""" File name : 9-add_item.py"""
+# 7-add_item.py
+""" File name : 7-add_item.py"""
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file('add_item.json')
